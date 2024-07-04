16126675ba95f590edd6e64d90cdb482e5fbd11e
Willy <158524087+Bsilvaa@users.noreply.github.com>
2024-07-03 21:39:53 -0300

    Add files via upload

---
+++ Will-python/Will.py
@@ -0,0 +1,24 @@
+#vamos comentar  nosso primeiro arquivo em python
+#print ('olá mundo,alerta de perigo para o domingo a noite')
+
+#idade = int(input('digite sua idade: '))
+
+#if idade <= 16:
+#print('voce e menor de idade')
+#else :
+ #print('voce e maior de idade')
+#se sua idade for menor de 16 (if) vai apresentar uma mensagem afirmando que vc e menor de idade caso contrário (else) apresentará uma mensagem afirmando que você e maior de idade.
+
+fetaria = int(input('digite sua idade: '))
+if fetaria > 18:
+    print('filme de terror')
+elif fetaria == 18:
+    print('filme de ação')
+elif fetaria > 14:
+   print('filme de ação')
+else:
+ print('filme infantil')
+
+  
+ 
+
---
+++ Will-python/lacoderepeticao.py
@@ -0,0 +1,14 @@
+#x = 0
+
+#while x < 5:
+ #   nome = input('qual é seu nome? ')
+ #  x = x +1
+
+x = 0 
+pessoas = []
+
+while x < 5:
+    nome = input('digite o nome de 5 animais com a inicial do seu nome: ')
+    pessoas.append(nome)
+    x = x+1
+    print(pessoas)
