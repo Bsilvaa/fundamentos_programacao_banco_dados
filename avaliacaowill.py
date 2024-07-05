918fdb208fb11e29f8ada24b56b1d526ac217c3f
Willy <158524087+Bsilvaa@users.noreply.github.com>
2024-07-04 21:32:09 -0300

    Add files via upload

---
+++ avaliacaowill.py
@@ -0,0 +1,12 @@
+valor_compra = float(input('digite o valor da compra: '))
+preco_final = calcular_preco_final(valor_compra)
+
+if valor_compra <=100:
+    desconto = 0
+    preco_final = valor_compra
+elif valor_compra <=200:
+    desconto =  
+    preco_final = valor_compra * (1 - desconto)
+elif valor_compra <=300:
+    desconto = 
+    preco_final = valor_compra * (1 - desconto)
